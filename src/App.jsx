3bf22fb7af396be325078586f5793c973331de05
Mohammed27800 <asmte2780@gmail.com>
2024-10-26 00:30:22 +0300

    add audio player

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 function App() {
   const [suwar, setSuwar] = useState([]);
-  const audioRef = useRef(null); // Create a ref for the audio element
+  const audioRef = useRef(null); // Ref for the AudioPlayer
   const suwrahAPI = "https://mp3quran.net/api/v3/suwar?language=ar";
 
   useEffect(() => {
@@ -24,29 +26,31 @@ function App() {
   const playAudio = (id) => {
     const index = getAudioIndex(id);
 
-    // Stop the currently playing audio if it exists
     if (audioRef.current) {
-      audioRef.current.pause();
-      audioRef.current.currentTime = 0; // Reset the audio time
+      audioRef.current.audio.current.src = `https://server11.mp3quran.net/a_jbr/${index}.mp3`;
+      audioRef.current.audio.current.play();
     }
-
-    // Create a new audio instance and play it
-    audioRef.current = new Audio(`https://server11.mp3quran.net/a_jbr/${index}.mp3`);
-    audioRef.current.play();
   };
 
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-9 content-around min-h-screen p-9 gap-6 bg-[#181A1B]">
-      <h1 className="text-3xl col-span-3 sm:col-span-9 text-center text-white">تلاوات بصوت الشيخ علي جابر رحمه الله</h1>
+    <div className="grid grid-cols-2 sm:grid-cols-9 content-around min-h-screen p-9 gap-6 bg-[#181A1B] mb-16 font-ArefRuqaa">
+      <h1 className="text-3xl col-span-2 sm:col-span-9 text-center text-white">
+        تلاوات بصوت الشيخ علي جابر رحمه الله
+      </h1>
       {suwar.map((sura) => (
         <button
           key={sura.id}
-          className={`bg-blue-600 p-3 rounded-lg`}
+          className="bg-blue-600 p-3 rounded-lg text-xl font-sans"
           onClick={() => playAudio(sura.id)} // Call playAudio instead
         >
-          {sura.id} {sura.name}
+          {sura.name}
         </button>
       ))}
+      <div className="col-span-2 sm:col-span-9 fixed bottom-0 left-0 right-0 mt-6">
+        <AudioPlayer ref={audioRef} />
+      </div>
+
+      <h1 className="col-span-2 sm:col-span-9 text-white text-4xl text-center">صدقة جارية لجدتي ليلى مهجري وجميع موتى المسملين</h1>
     </div>
   );
 }
