210f71e79c09b7501349506cfdf04123be704b2f
M7MD_dev1 <69204096+M7MDdev1@users.noreply.github.com>
2024-11-02 12:49:17 +0300

    add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,14 @@ function App() {
   const audioRef = useRef(null); // Ref for the AudioPlayer
   const suwrahAPI = "https://mp3quran.net/api/v3/suwar?language=ar";
   const [loading, setLoading] = useState(true);
-  const [searchTerm, setSearchTerm] = useState('');
-
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     axios
       .get(suwrahAPI)
       .then((res) => {
         setSuwar(res.data.suwar); // Ensure this matches the API response structure
-        setLoading(false)
+        setLoading(false);
       })
       .catch((error) => console.error(error));
   }, [suwrahAPI]);
@@ -38,31 +37,36 @@ function App() {
     }
   };
 
-  const filteredSuwar = suwar.filter(sura =>
-    sura.name.includes(searchTerm)
-  );
+  const filteredSuwar = suwar.filter((sura) => sura.name.includes(searchTerm));
 
   return (
-    <div className=" min-h-screen  bg-slate-600 mb-16 font-ArefRuqaa grid grid-cols-2 sm:grid-cols-3 gap-5 px-10 justify-items-center">
-      <div className="col-span-2 sm:col-span-3 flex flex-col">
-
-      <h1 className="text-3xl p-9 text-center text-white ">
-        تلاوات بصوت الشيخ علي جابر رحمه الله
-      </h1>
-      <input type="text" placeholder="...ابحث هنا" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} className="h-10 w-[80vw] rounded-3xl text-right text-xl text-gray-500 px-7 border-2 border-gray-400"/>
+    <div className="mb-16 grid min-h-screen grid-cols-2 justify-items-center gap-5 bg-slate-600 px-10 font-ArefRuqaa sm:grid-cols-3">
+      <div className="col-span-2 flex flex-col sm:col-span-3">
+        <h1 className="p-9 text-center text-3xl text-white">
+          تلاوات بصوت الشيخ علي جابر رحمه الله
+        </h1>
+        <input
+          type="text"
+          placeholder="...ابحث هنا"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="h-10 w-[80vw] rounded-3xl border-2 border-gray-400 px-7 text-right text-xl text-gray-500"
+        />
       </div>
-      {loading         ? Array.from({ length: 124 }).map((_, index) => <SkeletonLoader key={index} />)
-
-      :
-      
-      filteredSuwar.map((sura) => (
-  <Surah sura={sura} playAudio={playAudio} />
-      ))}
+      {loading
+        ? Array.from({ length: 124 }).map((_, index) => (
+            <SkeletonLoader key={index} />
+          ))
+        : filteredSuwar.map((sura) => (
+            <Surah sura={sura} playAudio={playAudio} />
+          ))}
       <div className="fixed bottom-0 left-0 right-0 mt-6">
         <AudioPlayer ref={audioRef} />
       </div>
 
-      <h1 className="h-32  text-4xl text-center p-4 col-span-2 sm:col-span-3 text-white">صدقة جارية لجدتي ليلى مهجري وجميع موتى المسملين</h1>
+      <h1 className="col-span-2 h-32 p-4 text-center text-4xl text-white sm:col-span-3">
+        صدقة جارية لجدتي ليلى مهجري وجميع موتى المسملين
+      </h1>
     </div>
   );
 }
--- src/Components/SkeletonLoader.tsx
+++ src/Components/SkeletonLoader.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export default function SkeletonLoader() {
   return (
-    <div className="bg-white py-1 px-2 font-sans flex justify-center w-full rounded-lg animate-pulse">
+    <div className="flex w-full animate-pulse justify-center rounded-lg bg-white px-2 py-1 font-sans">
       <div className="relative">
-        <div className="h-14 w-14 bg-gray-300 rounded-full mx-4"/>
-        <div className="absolute bg-gray-300 h-4 w-8 rounded top-1/4 left-1/2 -translate-x-1/2"/>
+        <div className="mx-4 h-14 w-14 rounded-full bg-gray-300" />
+        <div className="absolute left-1/2 top-1/4 h-4 w-8 -translate-x-1/2 rounded bg-gray-300" />
       </div>
-      <div className="flex flex-col justify-center text-sm text-left w-1/3">
-        <div className="bg-gray-300 h-6 w-2/3 rounded mb-2"/>
-        <div className="bg-gray-300 h-4 w-1/3 rounded"/>
+      <div className="flex w-1/3 flex-col justify-center text-left text-sm">
+        <div className="mb-2 h-6 w-2/3 rounded bg-gray-300" />
+        <div className="h-4 w-1/3 rounded bg-gray-300" />
       </div>
     </div>
-
   );
 }
